064c1e11ca2805960d513eafe60602aed816f37c
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2024-03-08 09:24:55 +0800

    Update some settings

--- query-gen.py
+++ query-gen.py
@@ -24,14 +24,16 @@ if __name__ == "__main__":
     except:
         print("Invalid input! Program terminated.")
         exit()
-    k = int(input("Enter kmax: "))
-    partial = float(input("Enter the length of the query windows (0 < x < 1, or -1 to generate random-length intervals): "))
+    km = int(input("Enter kmax: "))
+    partial = float(input("Enter the length of the query windows (0 < x < 1, or -1 to generate random-length intervals with fixed k): "))
     for i in range(num_of_queries):
         u, v = random.randint(1, n), random.randint(1, n)
         if partial == -1:
             length = int(tmax * random.random())
+            k = km
         else:
             length = int(tmax * partial)
+            k = random.randint(1, km)
         ts = random.randint(0, tmax - length)
-        contents += str(u) + " " + str(v) + " " + str(ts) + " " + str(ts + length) + " " + str(random.randint(1, k)) + " " + "\n"
+        contents += str(u) + " " + str(v) + " " + str(ts) + " " + str(ts + length) + " " + str(k) + " " + "\n"
     open("query.txt", "w").write(contents)
