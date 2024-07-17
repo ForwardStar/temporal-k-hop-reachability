ebba0e2cbc9a95c657cf5a30ed7b2eb8f7886eac
ForwardStar <haoxuanxie@link.cuhk.edu.cn>
2024-07-17 03:13:19 +0000

    Add interval queries

--- auto_run.py
+++ auto_run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 import subprocess
 
@@ -15,31 +16,63 @@ if __name__ == "__main__":
         os.makedirs("./results/%s" % (i))
         for k in [2, 4, 6]:
             os.makedirs("./results/%s/k=%s" % (i, k))
-            os.system("python3 query-gen.py 1000 %s -1" % (k))
-            try:
-                output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_online1.txt" % (i, k), "Online1", str(k), "Config"], timeout=259200)
-                with open("./results/%s/k=%s/online1.txt" % (i, k), "wb") as f:
-                    f.write(output)
-            except:
-                print("Timed out!")
-            try:
-                output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_online2.txt" % (i, k), "Online2", str(k), "Config"], timeout=259200)
-                with open("./results/%s/k=%s/online2.txt" % (i, k), "wb") as f:
-                    f.write(output)
-            except:
-                print("Timed out!")
-            try:
-                output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_MP.txt" % (i, k), "MP", str(k), "Config"], timeout=259200)
-                with open("./results/%s/k=%s/MP.txt" % (i, k), "wb") as f:
-                    f.write(output)
-            except:
-                print("Timed out!")
-            try:
-                output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_T2H.txt" % (i, k), "T2H", str(k), "Config"], timeout=259200)
-                with open("./results/%s/k=%s/T2H.txt" % (i, k), "wb") as f:
-                    f.write(output)
-            except:
-                print("Timed out!")
-            os.system("cmp ./results/%s/k=%s/output_online2.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
-            os.system("cmp ./results/%s/k=%s/output_MP.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
-            os.system("cmp ./results/%s/k=%s/output_T2H.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+            if sys.argv[len(sys.argv)-1] == "interval":
+                for partial in [0.2, 0.4, 0.6, 0.8]:
+                    os.system("python3 query-gen.py 1000 %s %s" % (k, partial))
+                    os.system("mv query.txt query_%s.txt" % (partial))
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query_0.2.txt", "query_0.4.txt", "query_0.6.txt", "query_0.8.txt", "./results/%s/k=%s/output_online1.txt" % (i, k), "Online1", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/online1.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query_0.2.txt", "query_0.4.txt", "query_0.6.txt", "query_0.8.txt", "./results/%s/k=%s/output_online2.txt" % (i, k), "Online2", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/online2.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query_0.2.txt", "query_0.4.txt", "query_0.6.txt", "query_0.8.txt", "./results/%s/k=%s/output_MP.txt" % (i, k), "MP", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/MP.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query_0.2.txt", "query_0.4.txt", "query_0.6.txt", "query_0.8.txt", "./results/%s/k=%s/output_T2H.txt" % (i, k), "T2H", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/T2H.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                os.system("cmp ./results/%s/k=%s/output_online2.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+                os.system("cmp ./results/%s/k=%s/output_MP.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+                os.system("cmp ./results/%s/k=%s/output_T2H.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+            else:
+                os.system("python3 query-gen.py 1000 %s -1" % (k))
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_online1.txt" % (i, k), "Online1", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/online1.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_online2.txt" % (i, k), "Online2", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/online2.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_MP.txt" % (i, k), "MP", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/MP.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                try:
+                    output = subprocess.check_output(["./main", "graph.txt", "query.txt", "./results/%s/k=%s/output_T2H.txt" % (i, k), "T2H", str(k), "Config"], timeout=259200)
+                    with open("./results/%s/k=%s/T2H.txt" % (i, k), "wb") as f:
+                        f.write(output)
+                except:
+                    print("Timed out!")
+                os.system("cmp ./results/%s/k=%s/output_online2.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+                os.system("cmp ./results/%s/k=%s/output_MP.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
+                os.system("cmp ./results/%s/k=%s/output_T2H.txt ./results/%s/k=%s/output_online1.txt" % (i, k, i, k))
